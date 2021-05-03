11eb723b236fa5bb7f675dcd2ce97a970a656c70
1ChanakyaUmrao <chanakya.umrao@successive.tech>
2021-05-03 20:10:48 +0530

    que7

---
+++ que7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int basenumber, exponent,power, i;
+            string ans;
+            Console.Write("Enter any basenumber: ");
+            basenumber = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.Write("Enter any exponent: ");
+            exponent = Convert.ToInt32(Console.ReadLine());
+ 
+            power = 1;
+            i = 1;
+            while (i <= exponent)
+            {
+                power = power * basenumber;
+                i++;
+            }
+            
+            Console.WriteLine("Power : "+ power);   
+            Console.WriteLine("Do you want to continue?");
+            ans = Console.ReadLine();
+            while(true)
+            {
+            if(ans == "y" || ans == "Y")
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Quit");
+            } 
+            }
+            
+        }
+    }
+}
