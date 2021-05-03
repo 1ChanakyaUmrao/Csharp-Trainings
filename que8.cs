85f5196b49c05b1c5d62fd1c5aa2114816dd8c5b
1ChanakyaUmrao <chanakya.umrao@successive.tech>
2021-05-03 20:25:18 +0530

    que8

---
+++ que8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j,n;
+            Console.Write("Input number of rows : ");
+            n = Convert.ToInt32(Console.ReadLine());    
+            for(i=0;i<=n;i++)
+            {
+            for(j=1;j<=n-i;j++)
+	        Console.Write(" ");
+            for(j=1;j<=i;j++)
+            Console.Write("{0}",j);
+            for(j=i-1;j>=1;j--)
+	        Console.Write("{0}",j);
+ 
+            Console.Write("\n");
+            
+        }
+    }
+    }
+}
