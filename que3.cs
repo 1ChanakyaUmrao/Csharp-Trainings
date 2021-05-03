6b5939d490fa5e1243a9e23001d17e19dff09ea8
1ChanakyaUmrao <chanakya.umrao@successive.tech>
2021-05-03 18:46:14 +0530

    que3

---
+++ que3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+        int basenumber, exponent, i, power;
+ 
+        Console.Write("Enter Base No.: ");
+        basenumber = Convert.ToInt32(Console.ReadLine());
+ 
+        Console.Write("Enter Exponent: ");
+        exponent = Convert.ToInt32(Console.ReadLine());
+ 
+        power = 1;        
+        for (i = 1; i <= exponent; i++)
+            power = power * basenumber;            
+ 
+        Console.Write("Power : "+ power);
+ 
+        Console.ReadLine();
+
+        }
+    }
+}
