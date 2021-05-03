671cd504ee0289f38d6c0af2f8798955fae00049
1ChanakyaUmrao <chanakya.umrao@successive.tech>
2021-05-03 18:57:20 +0530

    que4

---
+++ que4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double height;
+            double area;
+            Console.Write("Enter the base of triangle:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the height of triangle:");
+            height = Convert.ToDouble(Console.ReadLine());
+ 
+            area = (b * height) / 2;
+            Console.WriteLine("The area of your triangle is: {0}", area);
+        }
+    }
+}
