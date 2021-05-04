cab50782a1564256472588bfe0b11ff1aecc94e9
1ChanakyaUmrao <chanakya.umrao@successive.tech>
2021-05-04 10:08:43 +0530

    que6

---
+++ que6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            string option;
+           
+            Console.WriteLine("Choose book from the given options: ");
+            Console.WriteLine("c: computer books");
+            Console.WriteLine("m: mathematical books");
+            Console.WriteLine("h: history books");
+            Console.WriteLine("e: English books");
+
+            label:
+                Console.WriteLine("Enter your option:");
+                option = Console.ReadLine();
+
+            switch(option)
+            {
+                case "c":
+                    Console.WriteLine("COMPUTER BOOKS");
+                    break;
+                
+                case "m":
+                    Console.WriteLine("MATHEMATICAL BOOKS");
+                    break;
+                
+                case "h":
+                    Console.WriteLine("HISTORY BOOKS");
+                    break;
+
+                case "e":
+                    Console.WriteLine("ENGLISH BOOKS");
+                    break;
+
+                default:
+                    Console.WriteLine("Try Again");
+                    goto label;
+        }
+    }
+    }
+}
